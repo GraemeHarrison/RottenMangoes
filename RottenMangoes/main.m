3ec7a5fc1e0e77a932d18d44067393fb512370b9
Graeme Harrison <gharrison365@gmail.com>
2016-02-01 11:50:45 -0500

    First commit

---
+++ RottenMangoes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RottenMangoes
+//
+//  Created by Graeme Harrison on 2016-02-01.
+//  Copyright © 2016 Graeme Harrison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
