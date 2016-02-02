1f798a3ce35eb91d7c5d357dff41b62d84be427d
Graeme Harrison <gharrison365@gmail.com>
2016-02-02 11:36:09 -0500

    Added map view controller

---
+++ RottenMangoes/MapViewController.h
@@ -0,0 +1,14 @@
+//
+//  MapViewController.h
+//  RottenMangoes
+//
+//  Created by Graeme Harrison on 2016-02-02.
+//  Copyright © 2016 Graeme Harrison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
